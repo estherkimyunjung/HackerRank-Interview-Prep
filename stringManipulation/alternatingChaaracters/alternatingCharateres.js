f5d80525970145b150d176abecfb6cf12ef0e032
estherkimyunjung <estherkimyunjung@gmail.com>
2021-07-25 07:09:43 -0500

    alternating char

---
+++ stringManipulation/alternatingChaaracters/alternatingCharateres.js
@@ -0,0 +1,23 @@
+function alternatingCharacters(s) {
+  // Write your code here
+  // console.log(s.split(""))
+
+  let count = 0;
+  const arrPath = s.split("");
+
+  for (let i = 0; i < arrPath.length - 1; i++) {
+    if (
+      (arrPath[i] === "A" && arrPath[i + 1] === "A") ||
+      (arrPath[i] === "B" && arrPath[i + 1] === "B")
+    ) {
+      ++count;
+    }
+  }
+  return console.log(count);
+}
+
+alternatingCharacters("AAAA");
+alternatingCharacters("BBBBB");
+alternatingCharacters("ABABABA");
+alternatingCharacters("BABAB");
+alternatingCharacters("AABBAABB");
