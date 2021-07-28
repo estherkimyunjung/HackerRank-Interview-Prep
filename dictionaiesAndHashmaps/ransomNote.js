240f3020b1bf34f3c3cf9568e58c065b8465cdf3
estherkimyunjung <estherkimyunjung@gmail.com>
2021-07-28 16:28:46 -0500

    check magazine

---
+++ dictionaiesAndHashmaps/ransomNote.js
@@ -0,0 +1,17 @@
+function checkMagazine(magazine, note) {
+  // Write your code here
+
+  let wordKey = new Set(magazine);
+  for (let word of note) {
+    if (!wordKey.has(word)) {
+      return console.log("No");
+    }
+    wordKey.delete(word);
+  }
+  return console.log("Yes");
+}
+
+checkMagazine(
+  ["two", "times", "three", "is", "not", "four"],
+  ["two", "times", "two", "is", "four"]
+);
